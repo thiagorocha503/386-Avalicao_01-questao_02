2517f1635ba9e8d10589b0adf26fa65952903ecc
Thiago Rocha <thiagorocha503@gmail.com>
2019-10-19 14:33:23 -0300

    Alteração do cabeçalho, título e index do site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
 # Register your models here.
+admin.site.site_title = "Empresa de curso profissionalizante"
+admin.site.index_title = "Recursos"
+admin.site.site_header = "Administração da Empresa"
+
